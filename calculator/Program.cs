40cf6cfb85ed7c3f8962ec33c77321d3e4aea5b7
daniloid212 <79187282+daniloid212@users.noreply.github.com>
2021-12-27 23:55:16 +0300

    initial

---
+++ calculator/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Введите первое число ");
+            double Num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите знак операции ");
+            string Operation = Console.ReadLine();
+            Console.WriteLine("Введите второе число ");
+            double Num2 = Convert.ToDouble(Console.ReadLine());
+            double Result = double.NaN;
+
+            if (Operation == "+")
+                Result = Num1 + Num2;
+            else if (Operation == "-")
+                Result = Num1 - Num2;
+            else if (Operation == "*")
+                Result = Num1 * Num2;
+            else if (Operation == "/")
+                Result = Num1 / Num2;
+            Console.WriteLine(Result);
+        }
+    }
+}
+    
